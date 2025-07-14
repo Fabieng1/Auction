c43e770b19e997532a9712110098c1d5e513fa8c
fabien <fabien_gilles@hotmail.com>
2025-07-14 12:18:47 +0200

    Création d'un article à mettre aux enchères

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            String nameProductMain = "";
+            String categoryProductMain = "";
+            String descriptionProductionMain = "";
+
+            double priceProductMain = 0.0;
+
+            Product product = new Product(nameProductMain, categoryProductMain, descriptionProductionMain, priceProductMain);
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Quel est le nom de votre produit ? ");
+            nameProductMain = scanner.nextLine();
+
+            // Faire un enum des catégories
+            System.out.println("Dans quel catégorie est votre produit ?");
+            categoryProductMain = scanner.nextLine();
+
+            // Rendre obligatoire un nombre minimum de caractère
+            System.out.println("Entrez une descripion de votre produit ");
+            descriptionProductionMain = scanner.nextLine();
+
+            // Contraindre le prix selon le type d'enchère
+            System.out.println("Entrez le prix de votre produit ");
+            priceProductMain = scanner.nextDouble();
+
+
         }
-    }
 }
